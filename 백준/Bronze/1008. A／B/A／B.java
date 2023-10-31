5ecef2e38edcb4ed0b2b3b3cec2380e9e63b729c
Seo Ji Young <120671066+seojireung@users.noreply.github.com>
2023-11-01 00:49:21 +0900

    [Bronze V] Title: A/B, Time: 124 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,16 +1,20 @@
-import java.util.Scanner;
+// BufferedReader 
 
-public class Main{
-    public static void main(String[] args){
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
         
-        Scanner scan = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        
-        double A = scan.nextDouble();
-        double B = scan.nextDouble();
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str," ");
+        double A = Double.parseDouble(st.nextToken());
+        double B = Double.parseDouble(st.nextToken());
         
         System.out.println(A/B);
-        
-        scan.close();
     }
 }
