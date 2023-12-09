69f8f58629add84b3d40cd31feb0f8209d00e7a0
Seo Ji Young <120671066+seojireung@users.noreply.github.com>
2023-12-09 22:32:46 +0900

    [Bronze II] Title: 상수, Time: 120 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        
+		int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		
+		System.out.print(A > B ? A:B);
+		
+	}
+}
