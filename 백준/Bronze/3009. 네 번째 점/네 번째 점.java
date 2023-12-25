af174a936b429d487df2066a6351dc96e195fa12
Seo Ji Young <120671066+seojireung@users.noreply.github.com>
2023-12-25 23:35:29 +0900

    [Bronze III] Title: 네 번째 점, Time: 152 ms, Memory: 16040 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int x1 = Integer.parseInt(st.nextToken());
+		int y1 = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int x2 = Integer.parseInt(st.nextToken());
+		int y2 = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int x3 = Integer.parseInt(st.nextToken());
+		int y3 = Integer.parseInt(st.nextToken());
+		
+		int X = 0;
+		int Y = 0;
+
+		if(x1 == x2) {
+			X = x3;
+		}else if(x1 == x3) {
+			X = x2;
+		}else {
+			X = x1;
+		}
+		
+		if(y1 == y2) {
+			Y = y3;
+		}else if(y1 == y3) {
+			Y = y2;
+		}else {
+			Y = y1;
+		}
+		System.out.println(X + " " + Y);
+	}
+
+}
